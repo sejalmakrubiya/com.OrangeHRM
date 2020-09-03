1ba7e7cf668cc99850b778ff899089b3f9bf5a6c
sejalmakrubiya <sejaltest.123@gmail.com>
2020-09-03 16:44:31 +0100

    doing practice for git push

---
+++ src/LoginPage.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage {
+    public static void main(String[] args) {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver","C:\\shop\\sejal\\Sejal Testing\\software\\Chrome Driver\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        driver.findElement(By.id("btnLogin")).click();
+    }
+}
